9e01a7ac03cf4f97bc6ce5c484a75601773c3f6f
Justin Maronde <myemailisjustin@me.com>
2024-04-04 11:30:59 -0400

    add getPetTypes service and clean up app.tsx

--- App.tsx
+++ App.tsx
@@ -1,24 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import getAccessToken from './services/apiService';
 
 function App(): React.JSX.Element {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchAccessToken = async () => {
-      const token = await getAccessToken();
-      setAccessToken(token);
-    };
-
-    fetchAccessToken();
-  }, []);
-
   return (
     <SafeAreaView>
       <View>
         <Text>PetFinder</Text>
-        <Text>Access Token: {accessToken}</Text>
       </View>
     </SafeAreaView>
   );
--- services/apiService.ts
+++ services/apiService.ts
@@ -6,7 +6,10 @@ interface AccessTokenResponse {
     expires_in: number;
 }
 
+let jwt_access_token: string | null = null;
+
 const AUTH_ENDPOINT = 'https://api.petfinder.com/v2/oauth2/token';
+const TYPES_ENDPOINT = 'https://api.petfinder.com/v2/types';
 
 const getAccessToken = async (): Promise<string | null> => {
     try {
@@ -21,6 +24,7 @@ const getAccessToken = async (): Promise<string | null> => {
 
         const { access_token } = response.data;
         // You can store the access_token for future use here
+        jwt_access_token = access_token;
         return access_token;
     } catch (error) {
         console.error('Failed to get access token:', error);
@@ -28,4 +32,23 @@ const getAccessToken = async (): Promise<string | null> => {
     }
 };
 
-export default getAccessToken;
+const getPetTypes = async (): Promise<any> => {
+    if (!jwt_access_token) {
+        await getAccessToken();
+    }
+
+    try {
+        const response: AxiosResponse<any> = await axios.get(TYPES_ENDPOINT, {
+            headers: {
+                Authorization: `Bearer ${jwt_access_token}`,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error('Failed to get pet types:', error);
+        return [];
+    }
+};
+
+export default getPetTypes;
