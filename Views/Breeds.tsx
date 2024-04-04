69c9c42c01d0187fcc9704dfcc6fb3dc077f4403
Justin Maronde <myemailisjustin@me.com>
2024-04-04 14:43:54 -0400

    receiving the selected animal type name

--- Views/Breeds.tsx
+++ Views/Breeds.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {RouteProp} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
 
-const Breeds: React.FC = () => {
+type RootStackParamList = {
+  PetTypes: undefined;
+  Breeds: {petTypeName: string};
+};
+
+type BreedsScreenRouteProp = RouteProp<RootStackParamList, 'Breeds'>;
+type BreedsScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Breeds'
+>;
+type Props = {
+  route: BreedsScreenRouteProp;
+  navigation: BreedsScreenNavigationProp;
+};
+
+const Breeds: React.FC<Props> = ({route}) => {
+  const {petTypeName} = route.params;
   return (
     <View>
-      <Text>This is the Breeds Screen!.</Text>
+      <Text>This is the Breeds Screen! :: {petTypeName}</Text>
     </View>
   );
 };
