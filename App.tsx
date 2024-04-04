94dc5890c6cd743a314e15411b2e900ab96058d2
Justin Maronde <myemailisjustin@me.com>
2024-04-04 13:33:59 -0400

    add react-navigation and Breeds placeholder screen.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import PetTypes from './Views/PetTypes';
+import Breeds from './Views/Breeds';
+
+const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <PetTypes />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="PetTypes" component={PetTypes} />
+        <Stack.Screen name="BreedsScreen" component={Breeds} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Views/Breeds.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Breeds: React.FC = () => {
+  return (
+    <View>
+      <Text>This is the Breeds Screen!.</Text>
+    </View>
+  );
+};
+
+export default Breeds;
