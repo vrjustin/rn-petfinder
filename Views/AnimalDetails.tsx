ca3262b5b30b85c785ce8629852ca41f0d0f6e8e
Justin Maronde <myemailisjustin@me.com>
2024-04-08 10:41:15 -0400

    clean up : some issues with styles still but overall improved visually

--- Views/AnimalDetails.tsx
+++ Views/AnimalDetails.tsx
@@ -91,15 +91,13 @@ const AnimalDetails: React.FC<Props> = ({route}) => {
           </View>
         </ImageBackground>
       </View>
-      <Text>ID: {id}</Text>
-      <Text>OrgID: {organization_id}</Text>
-      <Text>Gender: {gender}</Text>
-      <Text>Size: {size}</Text>
-      <Text>Coat: {coat}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Status: {status}</Text>
-      <Text>URL: {url}</Text>
-      <Text style={{fontSize: 16, fontWeight: 'bold'}}>Gallery</Text>
+      <Text style={{padding: 8, fontWeight: 'bold', fontSize: '16'}}>
+        Introduction
+      </Text>
+      <Text style={{padding: 8, fontSize: '14'}}>{description}</Text>
+      <Text style={{padding: 8, fontSize: 16, fontWeight: 'bold'}}>
+        Gallery
+      </Text>
       <FlatList
         horizontal
         data={photos}
