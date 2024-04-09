65e42ee692b83b44dfa20cbe2dc2392d1783d964
Justin Maronde <myemailisjustin@me.com>
2024-04-09 07:57:41 -0400

    fix for duplicate ttf copy error. add in imagemock and configure jest to moduleNameMapper all images to that imageMock file.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '\\.(png|jpg|ico|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/ImageMock.tsx',
+  },
 };
