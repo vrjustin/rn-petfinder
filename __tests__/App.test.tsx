ec60a098c462fc7e8f5495a9c20df5b2a1cee3e4
Justin Maronde <myemailisjustin@me.com>
2024-04-09 10:47:47 -0400

    mock the initial screen that the navigator hits. we'll build snapshots for each component individually.

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -12,6 +12,8 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('../Views/PetTypes.tsx');
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
