bc4b7f4fa8265a537acfdb6baf3b2923389c3038
Justin Maronde <myemailisjustin@me.com>
2024-04-16 14:40:56 -0400

    enable flipper debugging so we can see state. confirmed once proper flipper plugin was installed.

--- stores/store.ts
+++ stores/store.ts
@@ -3,6 +3,8 @@ import petTypesReducer from '../reducers/petTypesReducer';
 import petBreedsReducer from '../reducers/petBreedsReducer';
 import animalsReducer from '../reducers/animalsReducer';
 
+const createDebugger = require('redux-flipper').default;
+
 const rootReducer = combineReducers({
   petTypes: petTypesReducer,
   petBreeds: petBreedsReducer,
@@ -13,6 +15,12 @@ export type RootState = ReturnType<typeof rootReducer>;
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    __DEV__
+      ? getDefaultMiddleware({serializableCheck: false}).concat(
+          createDebugger(),
+        )
+      : getDefaultMiddleware({serializableCheck: false}),
 });
 
 export default store;
