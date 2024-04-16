5a43846d3268741690b90dd991f536f276b93109
Justin Maronde <myemailisjustin@me.com>
2024-04-16 10:06:00 -0400

    tests are green. Need to investigate when that line for @babel/plugin-transform-modules-commonjs was added.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [['module:react-native-dotenv'],
-    '@babel/plugin-transform-modules-commonjs',
-  ],
+  plugins: [['module:react-native-dotenv']],
 };
