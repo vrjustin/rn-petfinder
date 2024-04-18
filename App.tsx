5a6aea0fde09e3e0fc370ec04ded493e8c520343
Justin Maronde <myemailisjustin@me.com>
2024-04-18 07:42:23 -0400

    add gear icon to Animals headerRight and console log out the press.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {TouchableOpacity} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Provider} from 'react-redux';
@@ -7,6 +8,7 @@ import PetTypes from './Views/PetTypes';
 import Breeds from './Views/Breeds';
 import Animals from './Views/Animals';
 import AnimalDetails from './Views/AnimalDetails';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createStackNavigator();
 
@@ -17,7 +19,24 @@ function App(): React.JSX.Element {
         <Stack.Navigator>
           <Stack.Screen name="PetTypes" component={PetTypes} />
           <Stack.Screen name="Breeds" component={Breeds} />
-          <Stack.Screen name="Animals" component={Animals} />
+          <Stack.Screen
+            name="Animals"
+            component={Animals}
+            options={{
+              headerRight: () => (
+                <>
+                  <TouchableOpacity onPress={console.log('Display Settings Screen')}>
+                    <FontAwesomeIcon
+                      name="gear"
+                      size={20}
+                      color="#000"
+                      style={{marginRight: 20}}
+                    />
+                  </TouchableOpacity>
+                </>
+              ),
+            }}
+          />
           <Stack.Screen name="AnimalDetails" component={AnimalDetails} />
         </Stack.Navigator>
       </NavigationContainer>
